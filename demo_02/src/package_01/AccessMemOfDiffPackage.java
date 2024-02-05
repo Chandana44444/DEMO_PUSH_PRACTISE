7f91975b7d0bac260d8a13edace2c4f5c3c79073
Chandana M <Chandana M@TYSS-Chandana-M>
2024-02-05 12:45:05 +0530

    1st

---
+++ demo_02/src/package_01/AccessMemOfDiffPackage.java
@@ -0,0 +1,14 @@
+package package_01;
+
+public class AccessMemOfDiffPackage {
+
+	public static void main(String[] args) {
+		MethodDemo m = new MethodDemo();
+		m.add2(30, 30);
+		//
+		//
+		//
+		//
+	}
+
+}
